7d80dce1a5cad0e63fa8eaf0ba31fb388fb9c08a
zhouhong12345 <15041916476@163.com>
2020-02-29 23:28:14 +0800

    htmlhint

---
+++ rectangle.js
@@ -0,0 +1,22 @@
+$(function(){
+  //get dom elem  
+  var $width  =$('#width'),
+      $height =$('#height'),
+      $btnCal =$('#calculate'),
+      $perimeter =$('#perimeter'),
+      $area   =$('#area');
+
+  /*calc button click event*/
+  $btnCal.click(function(){
+    //get value
+    var w=Number($width.val()),
+        h=Number($height.val());
+
+    //calculate
+    var p=2 * (w+h),
+        a=w * h;
+    //output
+    $perimeter.val(p);
+    $area.val(a);
+  }); 
+});
